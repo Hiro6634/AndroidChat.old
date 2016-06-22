29326c8cf98019370f62db5523deddde4eab1591
Hiro6634 <hiro.suyama@gmail.com>
2016-06-21 21:03:29 -0300

    Login Presenter

---
+++ app/src/main/java/edu/galileo/android/androidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,76 @@
+package edu.galileo.android.androidchat.login;
+
+/**
+ * Created by Hiro on 21/06/2016.
+ */
+public class LoginPresenterImpl implements LoginPresenter {
+    private LoginView loginView;
+    private LoginInteractor loginInteractor;
+
+    LoginPresenterImpl( LoginView loginView ){
+        this.loginView = loginView;
+//        this.loginInteractor = new
+    }
+    @Override
+    public void onDestroy() {
+        loginView = null;
+    }
+
+    @Override
+    public void checkForAuthentication() {
+        if( loginView != null ){
+            loginView.disableInputs();
+            loginView.showProgressBar();
+        }
+
+        loginInteractor.checSession();
+    }
+
+    @Override
+    public void validationLogin(String email, String password) {
+        if( loginView != null ){
+            loginView.disableInputs();
+            loginView.showProgressBar();
+        }
+
+        loginInteractor.doSignIn( email, password);
+    }
+
+    @Override
+    public void registerNewUser(String email, String password) {
+        if( loginView != null ){
+            loginView.disableInputs();
+            loginView.showProgressBar();
+        }
+
+        loginInteractor.doSignUp(email, password);
+    }
+
+    private void onSigInSuccess(){
+        if( loginView != null){
+            loginView.navigateToMainScreen();
+        }
+    }
+
+    private void onSigUpSuccess(){
+        if( loginView != null) {
+            loginView.newUserSuccess();
+        }
+    }
+
+    private void onSigInError(String error){
+        if( loginView != null) {
+            loginView.hideProgressBar();
+            loginView.enableInputs();
+            loginView.loginError(error);
+        }
+    }
+
+    private void onSigUpError(String error){
+        if( loginView != null) {
+            loginView.hideProgressBar();
+            loginView.enableInputs();
+            loginView.newUserError(error);
+        }
+    }
+}
