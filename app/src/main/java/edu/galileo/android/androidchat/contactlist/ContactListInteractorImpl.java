71bdc94846689be7fbb1c72b03b500a2c4596d74
Hiro6634 <hiro.suyama@gmail.com>
2016-07-01 10:36:29 -0300

    ContactList Presenter y Events

---
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListInteractorImpl.java
@@ -0,0 +1,26 @@
+package edu.galileo.android.androidchat.contactlist;
+
+/**
+ * Created by Hiro on 01/07/2016.
+ */
+public class ContactListInteractorImpl implements ContactListInteractor {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+}
