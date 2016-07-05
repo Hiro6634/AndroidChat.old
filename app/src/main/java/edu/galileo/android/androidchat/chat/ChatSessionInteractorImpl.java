078e4459c24eb6a195836ebf48a20a12c7cf2544
Hiro6634 <hiro.suyama@gmail.com>
2016-07-05 20:27:42 -0300

    Chat Presenter yevent

---
+++ app/src/main/java/edu/galileo/android/androidchat/chat/ChatSessionInteractorImpl.java
@@ -0,0 +1,11 @@
+package edu.galileo.android.androidchat.chat;
+
+/**
+ * Created by Hiro on 05/07/2016.
+ */
+public class ChatSessionInteractorImpl implements ChatSessionInteractor {
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
--- app/src/main/java/edu/galileo/android/androidchat/chat/events/ChatEvent.java
+++ app/src/main/java/edu/galileo/android/androidchat/chat/events/ChatEvent.java
@@ -1,7 +1,18 @@
 package edu.galileo.android.androidchat.chat.events;
 
+import edu.galileo.android.androidchat.entities.ChatMessage;
+
 /**
  * Created by Hiro on 04/07/2016.
  */
 public class ChatEvent {
+    ChatMessage message;
+
+    public ChatMessage getMessage() {
+        return message;
+    }
+
+    public void setMessage(ChatMessage message) {
+        this.message = message;
+    }
 }
